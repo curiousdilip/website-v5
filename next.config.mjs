f230894d253fb9c1c0fe0963cc5b45235fd4189d
MrDilipMaurya <curiousdilip@gmail.com>
2025-08-04 21:36:46 +0530

    something went wrong

--- next.config.mjs
+++ next.config.mjs
@@ -9,20 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-          { key: 'Content-Security-Policy', value: "default-src 'self'; img-src 'self' https://cdn.cosmicjs.com; script-src 'self';" },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
-          { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
