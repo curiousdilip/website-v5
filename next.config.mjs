21eac959f154112d847cf3319f2c5cc97b296e96
Dilip Kumar Maurya <curiousdilip@gmail.com>
2025-08-08 07:42:02 +0000

    disabled js for older browsers

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'nextSafe',
-                        value: 'isDev',
-                    },
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
-                    },
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'DENY',
-                    },
-                    {
-                        key: 'X-XSS-Protection',
-                        value: '1; mode=block',
-                    },
-                ],
-            },
-        ];
-    },
-    images: {
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'cdn.cosmicjs.com',
-            },
-          ],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "nextSafe",
+            value: "isDev",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
       },
-}
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.cosmicjs.com",
+      },
+    ],
+  },
+  experimental: {
+    legacyBrowsers: false,
+  },
+  swcMinify: true,
+};
 
 export default nextConfig;
