bd8ad6b0590d10cd9bbb120cb646281c2395108f
Dilip Kumar Maurya <curiousdilip@gmail.com>
2025-08-08 10:57:52 +0000

    google analytics connected

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,13 @@ import Footer from "@/components/Footer";
 import Head from "next/head";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Quicksand } from "next/font/google";
+import Script from "next/script";
 
 const quicksand = Quicksand({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600"],
   display: "swap",
-  variable: "--font-quicksand", 
+  variable: "--font-quicksand",
 });
 export default function MyApp({ Component, pageProps }) {
   const ref = useRef(null);
@@ -35,6 +36,18 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-W78VYNSQ67"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-W78VYNSQ67');
+        `}
+      </Script>
       <Header />
       <main data-scroll-container ref={ref} className={quicksand.className}>
         <Component {...pageProps} />
