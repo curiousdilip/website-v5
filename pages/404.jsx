e059b98a45902b454a9ec44286bf82eddedd96f9
MrDilipMaurya <curiousdilip@gmail.com>
2025-08-25 19:01:42 +0530

    package updated

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Header from '../components/Header';
 import { motion } from 'framer-motion';
 
 export default function Custom404() {
