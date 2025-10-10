263cfd4f09af39206eb3a5dd65d01898269c9bf8
MrDilipMaurya <curiousdilip@gmail.com>
2025-10-11 02:35:46 +0530

    website last updated text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -131,7 +131,7 @@ export default function Footer() {
 
           <motion.div className="copyright" variants={footerItemVariants}>
             <p>&copy; {new Date().getFullYear()} Dilip Kumar. All rights reserved. </p>
-            <p>Last updated by Dilip on 7 August, 2025 at 11:27 AM UTC +5:30</p>
+            <p>Last updated by Dilip on 11 October, 2025 at 2:32 AM UTC +5:30</p>
           </motion.div>
         </div>
       </motion.footer>
